b3fb5b43aaf62d2ddcdf7420bb6050f412a12d9a
Achthar <69119535+Achthar@users.noreply.github.com>
2023-05-12 22:00:00 +0200

    add flash loan logic

--- contracts/utils/AaveHandler.sol
+++ contracts/utils/AaveHandler.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.8.18;
 import "../external-protocols/aave-v3-core/interfaces/IPool.sol";
 import "../external-protocols/aave-v3-core/interfaces/IAToken.sol";
 import "../external-protocols/aave-v3-core/interfaces/IVariableDebtToken.sol";
+import "../external-protocols/aave-v3-core/protocol/libraries/configuration/ReserveConfiguration.sol";
 
 // flash loan implementation
 import "../interfaces/IFlashLoanReceiverAave.sol";
@@ -13,6 +14,8 @@ import "../interfaces/IFlashLoanReceiverAave.sol";
  * sets up Aave such that all operations can be conducted
  */
 contract AaveHandler is IFlashLoanSimpleReceiver {
+    using ReserveConfiguration for DataTypes.ReserveConfigurationMap;
+
     address immutable AAVE_POOL;
     address immutable ONE_INCH;
     // pair config
@@ -34,26 +37,36 @@ contract AaveHandler is IFlashLoanSimpleReceiver {
     function initializeAave(
         uint256 _amountCollateral,
         address _aTokenCollateral,
-        address _vTokenBorrow,
-        uint8 _eMode
+        address _vTokenBorrow
     ) internal {
         address aTokenCollateral = _aTokenCollateral;
         address vTokenBorrow = _vTokenBorrow;
         address assetCollateral = IAToken(_aTokenCollateral).UNDERLYING_ASSET_ADDRESS();
+        address assetBorrow = IVariableDebtToken(vTokenBorrow).UNDERLYING_ASSET_ADDRESS();
+
+        address pool = AAVE_POOL; // save gas
 
         COLLATERAL = assetCollateral;
-        BORROW = IVariableDebtToken(vTokenBorrow).UNDERLYING_ASSET_ADDRESS();
+        BORROW = assetBorrow;
         A_COLLATERAL = aTokenCollateral;
         V_BORROW = _vTokenBorrow;
 
         // transfer collateral from user
         IERC20(assetCollateral).transferFrom(msg.sender, address(this), _amountCollateral);
+
+        // approve for deposit and repayment
+        IERC20(assetCollateral).approve(pool, type(uint256).max);
+        IERC20(assetBorrow).approve(pool, type(uint256).max);
+
         // deposit to aave
-        IPool(AAVE_POOL).deposit(assetCollateral, _amountCollateral, address(this), 0);
+        IPool(pool).deposit(assetCollateral, _amountCollateral, address(this), 0);
+
+        // check eMode -> has to match
+        uint8 _eMode = validateEMode(assetCollateral, assetBorrow);
 
         // configure collateral
-        IPool(AAVE_POOL).setUserUseReserveAsCollateral(assetCollateral, true);
-        IPool(AAVE_POOL).setUserEMode(_eMode);
+        IPool(pool).setUserUseReserveAsCollateral(assetCollateral, true);
+        IPool(pool).setUserEMode(_eMode);
     }
 
     /**
@@ -70,33 +83,77 @@ contract AaveHandler is IFlashLoanSimpleReceiver {
         address vTokenBorrow = _vTokenBorrow;
 
         address assetCollateral = IAToken(_aTokenCollateral).UNDERLYING_ASSET_ADDRESS();
-
         COLLATERAL = assetCollateral;
         BORROW = IVariableDebtToken(vTokenBorrow).UNDERLYING_ASSET_ADDRESS();
         A_COLLATERAL = aTokenCollateral;
         V_BORROW = vTokenBorrow;
+
         IPool(AAVE_POOL).setUserEMode(_eMode);
     }
 
     // Flash loan call to open leveraged position
     function executeOperation(
-        address asset,
+        address,
         uint256 amount,
         uint256 premium,
         address initiator,
         bytes calldata params
     ) external override returns (bool) {
-        return false;
+        // validate initiator
+        require(initiator == address(this), "INVALID INITIATOR");
+        address pool = AAVE_POOL;
+        address collateral = COLLATERAL;
+
+        // decode params
+        (address target, bytes memory data, uint256 borrowAmount) = abi.decode(params, (address, bytes, uint256));
+
+        // deposit flashed reserve
+        IPool(pool).deposit(collateral, amount, address(this), 0);
+
+        // borrow target funds
+        IPool(pool).borrow(BORROW, borrowAmount, 2, 0, address(this));
+
+        // execute and check swap
+        (bool success, bytes memory result) = target.call(data);
+        require(success, "SWAP FAILED");
+
+        // decode amount received
+        uint256 amountReceived = abi.decode(result, (uint256));
+        uint256 amountToReturn = amount + premium;
+
+        // validate that the repayment can be moved forward with
+        require(amountReceived >= amountToReturn, "INSUFFICIENT REPAY BALLANCE");
+
+        // collect dust
+        uint256 dust;
+        unchecked {
+            dust = amountReceived - amountToReturn;
+        }
+
+        // deposit dust
+        if (dust > 0) IPool(pool).deposit(collateral, dust, address(this), 0);
+
+        return true;
     }
 
-    function openPosition(uint256 borrowAmountIn) internal {}
+    function openPosition(
+        uint256 targetCollateralAmount,
+        uint256 borrowAmount,
+        uint256,
+        address swapTarget,
+        bytes memory swapParams
+    ) internal {
+        bytes memory callData = abi.encode(swapTarget, swapParams, borrowAmount);
+        IPool(AAVE_POOL).flashLoanSimple(address(this), COLLATERAL, targetCollateralAmount, callData, 0);
+    }
 
-    function validateEMode(
-        uint8 _eMode,
-        address asset0,
-        address asset1
-    ) public view {
-        // DataTypes.EModeCategory memory data = IPool(AAVE_POOL).getEModeCategoryData(_eMode);
-        // data.
+    function validateEMode(address asset0, address asset1) public view returns (uint8 eMode) {
+        eMode = getEMode(asset0);
+        require(eMode == getEMode(asset1), "eModes mismatch");
+    }
+
+    function getEMode(address asset) public view returns (uint8 eMode) {
+        DataTypes.ReserveConfigurationMap memory config = IPool(AAVE_POOL).getConfiguration(asset);
+        eMode = uint8(config.getEModeCategory());
     }
 }
