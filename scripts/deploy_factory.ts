809ef5cbe0093325931060c395bcb02ee3d43e02
Achthar <69119535+Achthar@users.noreply.github.com>
2023-05-13 12:46:51 +0200

    add addresses

--- scripts/deploy_factory.ts
+++ scripts/deploy_factory.ts
@@ -45,3 +45,7 @@ main()
         console.error(error);
         process.exit(1);
     });
+
+// factory: 0x648cE75895873BECBC4c9a291A28CA1EF121953B
+// lens: 0xAe3C2d45270791Ef8aD023D1E66d275255db0499
+// entryPoint: 0x02567769aAD16E77f974c45080b66b3e42933331
