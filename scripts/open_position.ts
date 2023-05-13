94374cd17634f37d28ac3f36211d028e344712b4
Achthar <69119535+Achthar@users.noreply.github.com>
2023-05-13 19:04:49 +0200

    add api fetch on 1inch

--- scripts/open_position.ts
+++ scripts/open_position.ts
@@ -3,9 +3,20 @@ import { parseUnits } from 'ethers/lib/utils';
 import hre from 'hardhat'
 import { FiatWithPermit__factory, OneFXLens__factory, OneFXSlotFactory__factory } from '../types';
 import { coreAddresses } from './1FXAddresses';
-import { AGEUR_USDC, USDC_AGEUR, USDT_USDC } from './1inchResponses';
+import { AGEUR_USDC, USDC_AGEUR, USDT_USDC, USDT_USDC_5 } from './1inchResponses';
 import { addressesAaveATokens, addressesAaveVTokens, addressesTokens } from './aave_addresses';
+import axios from 'axios'
 
+
+async function fetchData(url: string) {
+    try {
+        const response = await axios.get(url);
+        console.log("response", response)
+        return response.data
+    } catch (error) {
+        console.log(error);
+    }
+}
 
 async function main() {
 
@@ -17,6 +28,7 @@ async function main() {
 
     console.log('Get factory')
     const factory = await new OneFXSlotFactory__factory(operator).attach(coreAddresses.factory[chainId])
+
 
     const collateralKey = 'USDC'
     const debtKey = 'USDT'
@@ -33,20 +45,30 @@ async function main() {
     const collateral = await new FiatWithPermit__factory(operator).attach(collateralAddress)
     const amountCollateral = parseUnits('1', 6)
 
-    const targetCollateral = parseUnits('9', 6)
 
-    const amountBorrow = parseUnits('10', 6).mul(101).div(100)
+    const borrowBase = '15'
 
 
+    const targetCollateral = parseUnits(borrowBase, 6).mul(99).div(100)
+
+    const amountBorrow = parseUnits(borrowBase, 6).mul(101).div(100)
     const projectedAddress = await factory.getNextAddress()
+
+    const USDC_USDT_LIVE: any = await fetchData(
+        `https://api.1inch.io/v5.0/137/swap?fromTokenAddress=${debtAddress}&toTokenAddress=${collateralAddress}&amount=${amountBorrow.toString()}&fromAddress=${projectedAddress}&slippage=1&destReceiver=${projectedAddress}&referrerAddress=${projectedAddress}&disableEstimate=true&compatibilityMode=true&burnChi=false&allowPartialFill=false&complexityLevel=0`
+    )
+
+
+
     console.log("Projected address", projectedAddress)
     const allowance = await collateral.allowance(operator.address, projectedAddress)
-   
+    console.log("Allowance on ", collateral.address, allowance.toString())
     // approve
     // if (allowance.gte(amountCollateral)) {
-    //     console.log("Approving")
-    //     const approveTx = await collateral.approve(projectedAddress, parseUnits('5', 6))
-    //     await approveTx.wait()
+        console.log("Approving")
+        const approveTx = await collateral.approve(projectedAddress, parseUnits(borrowBase, 6))
+        await approveTx.wait()
+        console.log("Approval done")
     // }
 
     await factory.connect(operator).estimateGas.createSlot(
@@ -56,7 +78,17 @@ async function main() {
         debtVTokenAddress,
         targetCollateral,
         amountBorrow,
-        USDT_USDC.tx.data,
+        USDC_USDT_LIVE.tx.data,
+    )
+
+    await factory.connect(operator).createSlot(
+        operator.address,
+        amountCollateral,
+        collateralATokenAddress,
+        debtVTokenAddress,
+        targetCollateral,
+        amountBorrow,
+        USDC_USDT_LIVE.tx.data,
     )
 
 
